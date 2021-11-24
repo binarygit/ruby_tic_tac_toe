de5f00fa4b876219db8e6e458ae7c4232b16c018
binarygit <Inscription.abhi@outlook.com>
2021-11-24 14:37:38 +0545

    create methods to make string red and green

---
+++ lib/string.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+end
