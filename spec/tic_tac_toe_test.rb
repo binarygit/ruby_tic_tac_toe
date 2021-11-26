7fb13f8adffc6ce29279617db118ac26ab7459cf
binarygit <Inscription.abhi@outlook.com>
2021-11-26 14:39:08 +0545

    write tests for verdict

--- spec/tic_tac_toe_test.rb
+++ spec/tic_tac_toe_test.rb
@@ -5,6 +5,7 @@ describe TicTacToe do
   let(:green_marker) { "\e[32m0\e[0m" }
 
   describe '#legal?' do
+
     # ordinary markers are ['1'..'9']
     context "when move is not an ordinary marker" do
       it 'returns false' do
@@ -66,5 +67,142 @@ describe TicTacToe do
         expect(subject).to be_legal
       end
     end
+  end
+
+  describe '#verdict' do
+
+    context 'when top row has same marker' do
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { red_marker }
+        subject.current_player = current_player
+        subject.markers_array = [red_marker, red_marker, red_marker, 4, 5, 6, 7, 8, 9]
+
+        expect(subject.verdict).to eq('win')
+      end
+
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { green_marker }
+        subject.current_player = current_player
+        subject.markers_array = [green_marker, green_marker, green_marker, 4, 5, 6, 7, 8, 9]
+
+        expect(subject.verdict).to eq('win')
+      end
+    end 
+
+    context 'when middle row has same marker' do
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { red_marker }
+        subject.current_player = current_player
+        subject.markers_array = [1, 2, 3, red_marker, red_marker, red_marker, 7, 8, 9]
+
+        expect(subject.verdict).to eq('win')
+      end
+
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { green_marker }
+        subject.current_player = current_player
+        subject.markers_array = [1, 2, 3, green_marker, green_marker, green_marker, 7, 8, 9]
+
+        expect(subject.verdict).to eq('win')
+      end
+    end 
+
+    context 'when bottom row has same marker' do
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { red_marker }
+        subject.current_player = current_player
+        subject.markers_array = [1, 2, 3, 4, 5, 6, red_marker, red_marker, red_marker]
+
+        expect(subject.verdict).to eq('win')
+      end
+
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { green_marker }
+        subject.current_player = current_player
+        subject.markers_array = [1, 2, 3, 4, 5, 6, green_marker, green_marker, green_marker]
+
+        expect(subject.verdict).to eq('win')
+      end
+    end 
+
+    context 'when left-to-right diagonal has same marker' do
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { red_marker }
+        subject.current_player = current_player
+        subject.markers_array = [red_marker, 2, 3, 4, red_marker, 6, 7, 8, red_marker]
+
+        expect(subject.verdict).to eq('win')
+      end
+
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { green_marker }
+        subject.current_player = current_player
+        subject.markers_array = [green_marker, 2, 3, 4, green_marker, 6, 7, 8, green_marker]
+
+        expect(subject.verdict).to eq('win')
+      end
+    end 
+
+    context 'when right-to-left diagonal has same marker' do
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { red_marker }
+        subject.current_player = current_player
+        subject.markers_array = [1, 2, red_marker, 4, red_marker, 6, red_marker, 8, 9]
+
+        expect(subject.verdict).to eq('win')
+      end
+
+      it 'returns win' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { green_marker }
+        subject.current_player = current_player
+        subject.markers_array = [1, 2, green_marker, 4, green_marker, 6, green_marker, 8, 9]
+
+        expect(subject.verdict).to eq('win')
+      end
+    end 
+
+    context 'when no identical markers have lined up in rows or diagonals and all ordinary markers are gone' do
+      it 'returns draw' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { green_marker }
+        subject.current_player = current_player
+        subject.markers_array = [green_marker, red_marker, red_marker, red_marker, red_marker, green_marker, green_marker, green_marker, red_marker]
+
+        expect(subject.verdict).to eq('draw')
+      end
+    end 
+
+    context 'when no moves have been made' do
+      it 'returns nil' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { red_marker }
+        subject.current_player = current_player
+
+        expect(subject.verdict).to be_nil
+      end
+    end 
+
+
+    context 'when no identical markers have lined up diagonal or otherwise and ordinary markers still remain' do
+      it 'returns nil' do
+        current_player = double('player')
+        allow(current_player).to receive(:marker) { red_marker }
+        subject.current_player = current_player
+        subject.markers_array = [1, red_marker, red_marker, red_marker, red_marker, green_marker, green_marker, green_marker, red_marker]
+
+        expect(subject.verdict).to be_nil
+      end
+    end 
+
   end
 end
