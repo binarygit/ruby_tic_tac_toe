fb154d4339fcc120f932ae41ac44733eb60dd4a4
binarygit <Inscription.abhi@outlook.com>
2021-12-02 14:08:43 +0545

    debug: crashed when saved_games dir didn't exist

--- lib/save.rb
+++ lib/save.rb
@@ -3,11 +3,15 @@
 module Save
   def save
     puts "\n If there are any save game files they will be listed below: \n\n"
-    Dir.children('saved_games').each_with_index { |file, index| puts " #{'-'} #{file}" }
+    display_saved_files if Dir.exist?('saved_games')
     puts "\n If you would like to create a new save file or overwrite one, write its name "
     print "\n Enter file name or number: "
     file_name = gets.chomp
     serialize(file_name)
+  end
+
+  def display_saved_files
+    Dir.children('saved_games').each_with_index { |file, index| puts " #{'-'} #{file}" } 
   end
 
   def serialize(file_name)
